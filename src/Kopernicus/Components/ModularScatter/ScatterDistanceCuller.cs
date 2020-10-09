32958915f5faae0b7e22e655de9e8441477d5d3e
R-T-B <21GunSoftware@comcast.net>
2020-10-09 12:15:18 -0700

    Release 41.

---
+++ src/Kopernicus/Components/ModularScatter/ScatterDistanceCuller.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Kopernicus.Components.ModularScatter
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    class ScatterDistanceCuller : MonoBehaviour
+    {
+        private MeshRenderer surfaceObject;
+        private void Start()
+        {
+            surfaceObject = GetComponent<MeshRenderer>();
+        }
+        private void Update()
+        {
+            float maxdistance = Kopernicus.RuntimeUtility.RuntimeUtility.KopernicusConfig.ScatterCullDistance;
+            float distance = Vector3.Distance(Camera.current.transform.position, surfaceObject.transform.position);
+
+            if (distance > maxdistance)
+            {
+                surfaceObject.enabled = false;
+            }
+            else
+            {
+                surfaceObject.enabled = true;
+            }
+        }
+    }
+}
